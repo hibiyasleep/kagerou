85be7da2afd8a2ef4b637c9f15c6a07057a0d72d
Kuriyama Hibiya <kuriyama@hibiya.moe>
2016-11-21 16:11:41 +0900

    respect config value's type

--- config/lib/ui.js
+++ config/lib/ui.js
@@ -20,7 +20,17 @@ window.addEventListener('load', function(e) {
 
   ;[].map.call($('input[data-config-key]'), _ => {
     let placeholder = _.getAttribute('placeholder')
-    _.value = config.get(_.getAttribute('data-config-key')) || ''
+    let value = config.get(_.getAttribute('data-config-key'))
+    let type = _.getAttribute('data-type')
+
+    if(type === 'array') {
+      _.value = value.join(', ')
+    } else if(type === 'boolean') {
+      _.checked = value
+    } else {
+      _.value = value || ''
+    }
+
     // placeholder glitch workaround
     _.setAttribute('placeholder', '')
     _.setAttribute('placeholder', placeholder)
@@ -46,7 +56,15 @@ window.addEventListener('load', function(e) {
 
   $('#save').addEventListener('click', _ => {
     [].map.call($('input[data-config-key]'), o => {
-      config.set(o.getAttribute('data-config-key'), o.value)
+      let value = o.value
+      let type = o.getAttribute('data-type')
+
+      if(type === 'array') {
+        value = value.split(o.getAttribute('data-splitter')).map(_ => _.trim())
+      } else if(type === 'boolean') {
+        value = o.checked
+      }
+      config.set(o.getAttribute('data-config-key'), value)
     })
 
     config.save()
