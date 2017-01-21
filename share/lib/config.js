681399e5fa72b7aff4998a1db032479fded32f3f
Kuriyama Hibiya <kuriyama@hibiya.moe>
2017-01-21 15:14:13 +0900

    restore '맑은 고딕' on fontconfig

--- share/lib/config.js
+++ share/lib/config.js
@@ -8,7 +8,7 @@ const CONFIG_DEFAULT = {
     // body
     'resize-factor': 1,
     'body-margin': '0.25rem',
-    'body-font': "'Roboto', 'Source Han Sans', 'Meiryo UI', 'MalgunGotinc', sans-serif",
+    'body-font': "'Roboto', 'Source Han Sans', 'Meiryo UI', '맑은 고딕', sans-serif",
     // header / ui
     'nav-opacity': 1,
     'nav-bg': 'rgba(31, 31, 31, 0.9)',
