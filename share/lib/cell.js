72307256bb70db297b712780f30ca71a56ede665
Kuriyama Hibiya <kuriyama@hibiya.moe>
2017-06-09 03:01:51 +0900

    3.4 korean zone names

---
+++ share/lib/cell.js
@@ -0,0 +1,10 @@
+// ACT throws localed string, even it's float or int
+const pFloat = _ => parseFloat(_.replace(',', '.'))
+
+export default {
+  formatter: {
+    pct: (val, conf) => {
+
+    }
+  }
+}
