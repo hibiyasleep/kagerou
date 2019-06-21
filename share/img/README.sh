52c3c4a8be9a64290cc850b970b32f066396689e
Kuriyama Hibiya <kuriyama@hibiya.moe>
2019-06-22 05:01:46 +0900

    fix icon position

--- share/img/README.sh
+++ share/img/README.sh
@@ -1,6 +1,6 @@
 cd class
 composite -background none -size 32x32 .png
-montage $(cat ../composition.tsv | sed 's/    /.png /g') -background none -tile 10x6 -geometry 32x32+0+0 classes.png
+montage $(cat ../composition.tsv | sed 's/\t/.png /g') -background none -tile 11x6 -geometry 32x32+0+0 classes.png
 rm .png
 pngquant classes.png
 mv classes-fs8.png ../classes.png
