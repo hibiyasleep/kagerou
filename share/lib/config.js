4341e0f2abfe7f3a1c82276dd8ed47d95cbb2b33
Kuriyama Hibiya <kuriyama@hibiya.moe>
2019-12-14 20:48:54 +0900

    v0.8.1 'end of world' - fix <small> on footer

--- share/lib/config.js
+++ share/lib/config.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const VERSION = '0.8.0'
-const CODENAME = 'Fireworks In The Summer End'
-const DESCRIPTION = '\'遠回りの先　これが最後の話\''
+const VERSION = '0.8.1'
+const CODENAME = 'end of world'
+const DESCRIPTION = 'FRENZY HOUSE'
 
 const CONFIG_DEFAULT = {
   lang: 'ko',
